ae32adb9723c0182b936b0610e2a01ddaf6d53c4
Diomedes Trinidad <diomedesinf39@hotmail.com>
2025-04-01 20:04:40 -0300

    Otra cosa mas

--- votes.py
+++ votes.py
@@ -1,6 +1,8 @@
-manzanas = 1/4
-naranjas = 2/3
-papas = 1/2
-kilos = manzanas + naranjas + papas
+import math
 
-print("La persona se lleva: ", kilos, " kilos.")
+valor_inicial = 1000000
+veinte_por_ciento = valor_inicial * 0.2
+minutos = 235
+horas = math.floor(minutos/ 60)
+print("La persona tiene una totalidad de: ")
+print(veinte_por_ciento * horas + valor_inicial)

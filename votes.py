b54674717d5a89c53a6fd06290af01ea4e9ea5ca
Diomedes Trinidad <diomedesinf39@hotmail.com>
2025-04-15 16:08:38 -0300

    Mejoras del programa

--- votes.py
+++ votes.py
@@ -2,12 +2,14 @@ inicio = int(input("Ingrese el numero de inicio: "))
 final = int(input("Ingrese el numero final: "))
 
 if inicio < final:
-    num = inicio
-    for i in range(final):
+    num = inicio + 1
+    for i in range(final - 2):
         print(num)
         num += 1
+elif inicio == final:
+    print("El inicio y el final son iguales :) ")
 else:
-    num = inicio
-    for i in range(inicio):
+    num = inicio - 1
+    for i in range(inicio - 2):
         print(num)
         num -= 1
